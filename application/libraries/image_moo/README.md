215afe1bf1a3ca4694f16997b65cc17032017068
katefrz <kate_freeze@yahoo.com>
2016-11-28 22:16:53 +0700

    #new CI

--- application/libraries/image_moo/README.md
+++
@@ -1,6 +0,0 @@
-image_moo
-=========
-
-image_moo (PHP Image manipulation class)
-
-http://www.matmoo.com/digital-dribble/codeigniter/image_moo/
--- application/views/common/footer.php
+++
@@ -1,4 +0,0 @@
-    <div class="footer text-center navbar-fixed-bottom">
-        &copy; 2016 COT. All right reserved &middot; <a href="<?php echo site_url(); ?>">Privacy & Terms</a>
-    </div>
-</html>
